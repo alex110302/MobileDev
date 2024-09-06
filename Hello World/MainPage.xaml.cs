b0bf77bf7951310acf5ff3f632cb63c402f4bb33
alex110302 <hansenaalexander@gmail.com>
2024-09-06 12:51:14 -0700

    First App Hello World

---
+++ Hello World/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Hello_World;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void Hello_OnClicked(object sender, EventArgs e)
+    {
+        if (lblDisplay.Text == "Hello World") lblDisplay.Text = "this is cool!";
+        else lblDisplay.Text = "Hello World";
+    }
+}
